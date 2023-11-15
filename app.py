30152104f92d76c41d1de3cbbc8241f7dc893050
devphase <ayomidelawal700@gmail.com>
2023-11-15 11:25:01 +0000

    fix app crash

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ import streamlit as st
 
 model = joblib.load('./RF_model.pkl')
 
+
 def predict(facts):
-    input = facts
+    input = [facts]
     prediction = model.predict(input)
 
-def main():
-    st.title('Court Facts')
 
-    facts= st.text_input('Enter your facts',)
+def main():
+    st.title('Sepreme Court Judgement Predicition')
+
+    facts = st.text_input('Enter your facts',)
 
     if st.button('Predict facts'):
         output = predict(facts)
-        if (output== 0):
-            st.write('False')
+        if (output == 0):
+            st.write('First Party wins')
         else:
-            st.write('True')
-if __name__=='__main__':
+            st.write('Seond Party wins')
+
+
+if __name__ == '__main__':
     main()
